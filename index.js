df3f190d0bfcbd5b55f3106348ca91a8fd651fad
KHUSHBOO CHAURASIYA <98606333+Khushboo-25@users.noreply.github.com>
2024-03-26 17:50:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
-var n1 =  Math.floor(1+(6*Math.random())) ; //1-6
-var n2 =  Math.floor(1+(6*Math.random())) ;
+var randomNumber1 =  Math.floor(1+(6*Math.random())) ; //1-6
+var randomNumber2 =  Math.floor(1+(6*Math.random())) ;
 
-var randomImageSource1 = "images/dice" + n1 + ".png";
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
-var randomImageSource2 = "images/dice" + n2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
-if (n1 ==n2) {
+if (randomNumber1 ==randomNumber2) {
   
   document.querySelector("h1").innerHTML = "Draw!";
   
 }
-else if (n2 > n1) {
+else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
